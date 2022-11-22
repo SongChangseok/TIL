ae8f51842302c7bb411950eea606f3370f793a89
송창석 <31471148+SongChangseok@users.noreply.github.com>
2022-11-22 23:14:00 +0900

    Delete index.js

--- Algorithm/Baekjoon/index.js
+++
@@ -1,7 +0,0 @@
-// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-const input = require("fs").readFileSync("example.txt").toString().split("\n");
-const a = parseInt(input[0]);
-const b = parseInt(input[1]);
-
-console.log(a + b);
-asd;
