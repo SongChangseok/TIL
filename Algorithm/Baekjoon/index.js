7063968c61ec066daebe674fa1e30ae288a4eb22
cssong <a1373052011@gmail.com>
2022-11-22 22:57:23 +0900

    ss

--- Algorithm/Baekjoon/index.js
+++ Algorithm/Baekjoon/index.js
@@ -1,3 +1,4 @@
+// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 const input = require("fs").readFileSync("example.txt").toString().split("\n");
 const a = parseInt(input[0]);
 const b = parseInt(input[1]);
