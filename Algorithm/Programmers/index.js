264db304233e1364afdf02a8be42222984a763fa
cssong <a1373052011@gmail.com>
2023-01-04 11:46:02 +0900

    프로그래머스 12939

--- Algorithm/Programmers/index.js
+++ Algorithm/Programmers/index.js
@@ -1,6 +1,11 @@
-// const s = "banana";
-// const s = "abracadabra";
-const s = "aaabbaccccabba";
+// const s = "1 2 3 4";
+const s = "-1 -2 -3 -4";
+// const s = "-1 -1";
 
 console.log(solution(s));
 
+function solution(s) {
+  let array = s.split(" ");
+
+  return `${Math.min(...array)} ${Math.max(...array)}`;
+}
