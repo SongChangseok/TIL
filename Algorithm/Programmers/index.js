ec2c91261041f06f22673aeb79cbba511ac18d56
cssong <a1373052011@gmail.com>
2023-01-05 11:05:27 +0900

    프로그래머스 64065

--- Algorithm/Programmers/index.js
+++ Algorithm/Programmers/index.js
@@ -1,11 +1,23 @@
-// const s = "1 2 3 4";
-const s = "-1 -2 -3 -4";
-// const s = "-1 -1";
+// const s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+const s = "{{1,2,3},{2,1},{1,2,4,3},{2}}";
+// const s = "{{20,111},{111}}";
+// const s = "{{123}}";
+// const s = "{{4,2,3},{3},{2,3,4,1},{2,3}}";
 
 console.log(solution(s));
 
 function solution(s) {
-  let array = s.split(" ");
-
-  return `${Math.min(...array)} ${Math.max(...array)}`;
+  return s
+    .replace("{{", "")
+    .replace("}}", "")
+    .split("},{")
+    .sort((a, b) => a.length - b.length)
+    .reduce(
+      (acc, cur) => [
+        ...acc,
+        ...cur.split(",").filter((value) => !acc.includes(value)),
+      ],
+      []
+    )
+    .map((value) => parseInt(value));
 }
