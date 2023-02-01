e858d62482151d96486bbff855d330731f2c5cc7
cssong <a1373052011@gmail.com>
2023-02-01 15:59:31 +0900

    udemy 문제 해결 패턴에 대한 학습

---
+++ Algorithm&DataStructures/countUniqueValues.js
@@ -0,0 +1,6 @@
+const countUniqueValues = (array) => new Set(array).size;
+
+countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
+countUniqueValues([]); // 0
+countUniqueValues([-2, -1, -1, 0, 1]); // 4
